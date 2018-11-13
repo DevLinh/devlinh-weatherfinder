dca29643c0e50fc72e598ad9ddc69ac2dd052218
DevLinh <devlinh99qb@gmail.com>
2018-11-13 21:43:06 +0700

    Updates

---
+++ precache-manifest.b78cd57bd6653aeb87f5cee15a5a50f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/xemthoitiet/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "25402f843c80306a35e1",
+    "url": "/xemthoitiet/static/css/main.70402a4f.chunk.css"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/xemthoitiet/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "7b70167d3f6a27e50d26",
+    "url": "/xemthoitiet/static/js/1.7b70167d.chunk.js"
+  },
+  {
+    "revision": "44a2e6be484af67e0d14",
+    "url": "/xemthoitiet/static/js/runtime~main.44a2e6be.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/xemthoitiet/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "25402f843c80306a35e1",
+    "url": "/xemthoitiet/static/js/main.25402f84.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/xemthoitiet/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/xemthoitiet/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/xemthoitiet/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "7b70167d3f6a27e50d26",
+    "url": "/xemthoitiet/static/css/1.fd415fc2.chunk.css"
+  },
+  {
+    "revision": "6709ec4bb7dadb12846c4535bb980946",
+    "url": "/xemthoitiet/index.html"
+  }
+];
